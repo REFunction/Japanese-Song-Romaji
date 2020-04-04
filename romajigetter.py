dd0169320d4d98384c91a19095f77b042540235d
R.E Function <806675223@qq.com>
2020-04-05 07:59:27 +0800

    Add files via upload

--- romajigetter.py
+++ romajigetter.py
@@ -127,6 +127,7 @@ class RomajierGetter:
             romajis[i] = romajis[i].replace('-', ' ')
             romajis[i] = romajis[i].replace('`', '')
             romajis[i] = romajis[i].replace('dzu', 'zu')
+            romajis[i] = romajis[i].replace('tchi', 'chi')
             romajis[i] = romajis[i].replace('ō', 'ou')
             romajis[i] = romajis[i].replace('ū', 'uu')
             romajis[i] = romajis[i].replace('ī', 'ii')
